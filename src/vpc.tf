d829a5786609d01d058d6392d519258b37ca9c0a
Cory O'Daniel <cory@coryodaniel.com>
2022-06-07 12:15:30 -0700

    removing obs blocks

--- src/vpc.tf
+++ src/vpc.tf
@@ -6,29 +6,3 @@ resource "aws_vpc" "main" {
     Name = var.md_metadata.name_prefix
   }
 }
-
-resource "aws_sns_topic" "cloudwatch_alarms" {
-  name = "${var.md_metadata.name_prefix}-alarms"
-  # https://docs.aws.amazon.com/sns/latest/dg/sns-message-delivery-retries.html
-  delivery_policy = <<EOF
-  {
-    "http": {
-      "defaultHealthyRetryPolicy": {
-        "numNoDelayRetries": 5,
-        "minDelayTarget": 1,
-        "numMinDelayRetries": 5,
-        "maxDelayTarget": 3,
-        "numMaxDelayRetries": 5,
-        "numRetries": 30
-      },
-      "disableSubscriptionOverrides": false
-    }
-  }
-  EOF
-}
-
-resource "aws_sns_topic_subscription" "massdriver_cloudwatch_alarm_subscription" {
-  endpoint  = var.md_metadata.observability.alarm_webhook_url
-  protocol  = "https"
-  topic_arn = aws_sns_topic.cloudwatch_alarms.arn
-}
