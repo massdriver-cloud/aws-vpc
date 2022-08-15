d3416784b4f9aa171458e2138e57b1d8cb65ad17
Will Beebe <will@containerlabs.io>
2022-08-15 10:43:28 -0700

    Region dropdown (#15)
    * using region dropdown widget
    
    * updating precommit, other files

--- src/azs.tf
+++ src/azs.tf
@@ -1,8 +1,8 @@
 locals {
-// Limiting each list to a maximum of 4 entries for 3 reasons:
-// * The maximum EIPs per region is 5, so we must stay below 5 so our HA VPCs can deploy w/out a quota increase
-// * Provisioning one of our VPCs won't exhaust all EIPs in a region with 5+ AZs
-// * The CIDR math for 4 cleaner than 5
+  // Limiting each list to a maximum of 4 entries for 3 reasons:
+  // * The maximum EIPs per region is 5, so we must stay below 5 so our HA VPCs can deploy w/out a quota increase
+  // * Provisioning one of our VPCs won't exhaust all EIPs in a region with 5+ AZs
+  // * The CIDR math for 4 cleaner than 5
   fetched_azs = length(data.aws_availability_zones.azs.names) <= 4 ? data.aws_availability_zones.azs.names : slice(data.aws_availability_zones.azs.names, 0, 4)
 }
 
