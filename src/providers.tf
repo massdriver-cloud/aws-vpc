6ee166615486decdd1d5940832e50c55839b48f6
chrisghill <chris@massdriver.cloud>
2026-06-10 16:16:49 -0600

    remove 'data' top level field on aws-iam-role

--- src/providers.tf
+++ src/providers.tf
@@ -23,8 +23,8 @@ terraform {
 provider "aws" {
   region = var.aws_region
   assume_role {
-    role_arn    = var.aws_authentication.data.arn
-    external_id = var.aws_authentication.data.external_id
+    role_arn    = var.aws_authentication.arn
+    external_id = var.aws_authentication.external_id
   }
   default_tags {
     tags = var.md_metadata.default_tags
